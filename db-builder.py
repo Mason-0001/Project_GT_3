523c39ce5e710c343509aec575d104cf31e5af2f
Jack Fitzgerald <jffitzgerald@crimson.ua.edu>
2023-03-30 21:40:00 -0400

    cleaning database

--- db-builder.py
+++ db-builder.py
@@ -10,12 +10,19 @@ engine = create_engine(f"sqlite:///{database_path}")
 # Establisting a connection to our database
 conn = engine.connect()
 
-
-
 tornado_df = pd.read_csv('data/1950-2021_actual_tornadoes.csv')
 
-tornado_df.to_sql("tornado", conn)
+tornado_clean = tornado_df[['yr', 'mo', 'dy', 'date', 'time', 'st', 'mag', 'inj', 'fat', 'loss', 'closs', 'slat', 'slon', 
+                            'elat', 'elon', 'len', 'wid']]
 
+tornado_clean = tornado_clean[tornado_clean['yr'] >= 2007] 
+
+tornado_clean = tornado_clean[tornado_clean['mag'] != -9] 
+
+tornado_clean.columns = ['year', 'month', 'day', 'date', 'time', 'state', 'EF scale', 'injuries', 'fatalities', 'property loss $M', 'crop loss', 'starting latitude', 'starting longitude', 
+                         'ending latitude', 'ending longitude', 'length in miles', 'width in yards']
+
+tornado_clean.to_sql("tornado", conn)
 
 
 
@@ -24,3 +31,5 @@ tornado_df.to_sql("tornado", conn)
 
 
 engine.dispose()
+
+
