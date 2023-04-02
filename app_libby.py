7619d81a87056d5a07fe3bb4366a2d634ae492a8
libbyberter@gmail.com <118197596+libbyberter@users.noreply.github.com>
2023-04-02 13:06:34 -0400

    upload test files 04-02-2023

---
+++ app_libby.py
@@ -0,0 +1,154 @@
+from flask import Flask, render_template, redirect, jsonify
+import csv
+import sqlite3
+import pandas as pd
+from sqlalchemy import create_engine
+
+# import json
+
+app = Flask(__name__)
+
+
+
+
+
+# web route
+@app.route('/')
+def home():
+    return render_template("index_libby.html")
+
+
+
+
+
+
+# API routes
+
+@app.route("/api/geojson")
+def test():
+        # Path to SQLite file
+    database_path = "data/tornado_db.sqlite"
+
+    # Creating the SQL database
+    engine = create_engine(f"sqlite:///{database_path}")
+
+    # Establisting a connection to our database
+    conn = engine.connect()
+
+    
+    
+    tornado_df = pd.read_sql("SELECT * from tornado where [yr] > 2015", conn)
+
+    response = tornado_df.to_dict(orient="records")
+
+        # Create a GeoJSON FeatureCollection
+    features = []
+    for tornado in response:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [tornado["elon"], tornado["elat"]]
+            },
+            "properties": {
+                "closs": tornado["closs"],
+                "date": tornado["date"],
+                "dy": tornado["dy"],
+                "elat": tornado["elat"],
+                "elon": tornado["elon"],
+                "f1": tornado["f1"],
+                "f2": tornado["f2"],
+                "f3": tornado["f3"],
+                "f4": tornado["f4"],
+                "fat": tornado["fat"],
+                "fc": tornado["fc"],
+                "index": tornado["index"],
+                "inj": tornado["inj"],
+                "len": tornado["len"],
+                "loss": tornado["loss"],
+                "mag": tornado["mag"],
+                "mo": tornado["mo"],
+                "ns": tornado["ns"],
+                "om": tornado["om"],
+                "sg": tornado["sg"],
+                "slat": tornado["slat"],
+                "slon": tornado["slon"],
+                "sn": tornado["sn"],
+                "st": tornado["st"],
+                "stf": tornado["stf"],
+                "stn": tornado["stn"],
+                "time": tornado["time"],
+                "tz": tornado["tz"],
+                "wid": tornado["wid"],
+                "yr": tornado["yr"]
+            }
+        }
+        features.append(feature)
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    return jsonify(geojson)
+
+
+
+@app.route("/api/json")
+def statistics():
+        # Path to SQLite file
+    database_path = "data/tornado_db.sqlite"
+
+    # Creating the SQL database
+    engine = create_engine(f"sqlite:///{database_path}")
+
+    # Establisting a connection to our database
+    conn = engine.connect()
+
+    
+    
+    tornado_df = pd.read_sql("SELECT yr, date, inj, loss, mag, fat, wid, len from tornado where [yr] > 2015", conn)
+
+    response = tornado_df.to_dict(orient="records")
+    return jsonify(response)
+
+
+@app.route("/api/time")
+def timechart():
+        # Path to SQLite file
+    database_path = "data/tornado_db.sqlite"
+
+    # Creating the SQL database
+    engine = create_engine(f"sqlite:///{database_path}")
+
+    # Establisting a connection to our database
+    conn = engine.connect()
+
+    
+    
+    tornado_df = pd.read_sql("SELECT yr as Year, count(mag) as Tornados, avg(mag) as Magnitude, avg(loss) as Loss from tornado where [yr] >= 2000 group by yr", conn)
+
+    response = tornado_df.to_dict(orient="records")
+    return jsonify(response)
+
+
+@app.route("/api/state")
+def statechart():
+        # Path to SQLite file
+    database_path = "data/tornado_db.sqlite"
+
+    # Creating the SQL database
+    engine = create_engine(f"sqlite:///{database_path}")
+
+    # Establisting a connection to our database
+    conn = engine.connect()
+
+    
+    
+    tornado_df = pd.read_sql("SELECT yr as Year, st as State, count(mag) as Tornados, avg(mag) as Magnitude, avg(loss) as Loss from tornado where [yr] >= 2000 group by yr, st", conn)
+
+    response = tornado_df.to_dict(orient="records")
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
