dbb414293b441621146942627980c99a2b66967e
libbyberter@gmail.com <118197596+libbyberter@users.noreply.github.com>
2023-04-03 23:15:37 -0400

    working charts page as of 4-3-23

--- app_libby.py
+++ app_libby.py
@@ -34,10 +34,8 @@ def test():
 
     # Establisting a connection to our database
     conn = engine.connect()
-
     
-    
-    tornado_df = pd.read_sql("SELECT * from tornado where [yr] > 2015", conn)
+    tornado_df = pd.read_sql("SELECT * from tornado where [year] > 2006", conn)
 
     response = tornado_df.to_dict(orient="records")
 
@@ -48,39 +46,25 @@ def test():
             "type": "Feature",
             "geometry": {
                 "type": "Point",
-                "coordinates": [tornado["elon"], tornado["elat"]]
+                "coordinates": [tornado["starting latitude"], tornado["starting longitude"]]
             },
             "properties": {
-                "closs": tornado["closs"],
+                "year": tornado["year"],
+                "month": tornado["month"],
+                "day": tornado["day"],
                 "date": tornado["date"],
-                "dy": tornado["dy"],
-                "elat": tornado["elat"],
-                "elon": tornado["elon"],
-                "f1": tornado["f1"],
-                "f2": tornado["f2"],
-                "f3": tornado["f3"],
-                "f4": tornado["f4"],
-                "fat": tornado["fat"],
-                "fc": tornado["fc"],
-                "index": tornado["index"],
-                "inj": tornado["inj"],
-                "len": tornado["len"],
-                "loss": tornado["loss"],
-                "mag": tornado["mag"],
-                "mo": tornado["mo"],
-                "ns": tornado["ns"],
-                "om": tornado["om"],
-                "sg": tornado["sg"],
-                "slat": tornado["slat"],
-                "slon": tornado["slon"],
-                "sn": tornado["sn"],
-                "st": tornado["st"],
-                "stf": tornado["stf"],
-                "stn": tornado["stn"],
-                "time": tornado["time"],
-                "tz": tornado["tz"],
-                "wid": tornado["wid"],
-                "yr": tornado["yr"]
+                "state": tornado["state"],
+                "EFscale": tornado["EF scale"],
+                "injuries": tornado["injuries"],
+                "fatalities": tornado["fatalities"],
+                "property loss $": tornado["property loss $"],
+                "crop loss": tornado["crop loss"],
+                "slat": tornado["starting latitude"],
+                "slon": tornado["starting longitude"],
+                "ending latitude": tornado["ending latitude"],
+                "ending longitude": tornado["ending longitude"],
+                "length in miles": tornado["length in miles"],
+                "width in yards": tornado["width in yards"],
             }
         }
         features.append(feature)
@@ -93,7 +77,6 @@ def test():
     return jsonify(geojson)
 
 
-
 @app.route("/api/json")
 def statistics():
         # Path to SQLite file
@@ -104,10 +87,8 @@ def statistics():
 
     # Establisting a connection to our database
     conn = engine.connect()
-
-    
-    
-    tornado_df = pd.read_sql("SELECT yr, date, inj, loss, mag, fat, wid, len from tornado where [yr] > 2015", conn)
+   
+    tornado_df = pd.read_sql("SELECT year, date, injuries, [property loss $], [EF scale], fatalities, [width in yards], [length in miles] from tornado where [year] > 2006", conn)
 
     response = tornado_df.to_dict(orient="records")
     return jsonify(response)
@@ -123,10 +104,8 @@ def timechart():
 
     # Establisting a connection to our database
     conn = engine.connect()
-
-    
-    
-    tornado_df = pd.read_sql("SELECT yr as Year, count(mag) as Tornados, avg(mag) as Magnitude, avg(loss) as Loss from tornado where [yr] >= 2007 group by yr order by yr", conn)
+ 
+    tornado_df = pd.read_sql("SELECT year as Year, count([EF scale]) as Tornados, avg([EF scale]) as Magnitude, avg([property loss $]) as Loss from tornado where [year] > 2006 group by year order by year", conn)
 
     response = tornado_df.to_dict(orient="records")
     return jsonify(response)
@@ -145,7 +124,7 @@ def statechart():
 
     
     
-    tornado_df = pd.read_sql("SELECT yr as Year, st as State, count(mag) as Tornados, avg(mag) as Magnitude, avg(loss) as Loss from tornado where [yr] >= 2007 group by yr, st order by yr", conn)
+    tornado_df = pd.read_sql("SELECT year as Year, state as State, count([EF scale]) as Tornados, avg([EF scale]) as Magnitude, avg([property loss $]) as Loss from tornado where [year] >= 2007 group by year, state order by year", conn)
 
     response = tornado_df.to_dict(orient="records")
     return jsonify(response)
--- static/js/main_libby.js
+++ static/js/main_libby.js
@@ -1,163 +1,90 @@
-// Store the API endpoint as queryUrl.
-var queryUrl = "/api/geojson"
 
-// // Perform a GET request to the query URL/
-// d3.json(queryUrl).then(function (data) {
-// //   Once we get a response, send the data.features object to the createFeatures function.
-//   createFeatures(data);
 
-// });
+// set inital Table to year 2021 & initial chart to tornados
+yearSel = createYearSelector();
+var selectedYear = 2021
+var selectedChart = 1
+var ChartNew = 0
+var timeChartNew = 0
+getChartJson();
+getTimeJson();
+getStateJson();
 
-// function createFeatures(tornadoData) {
-//     console.log(tornadoData)
-// // Define a function that we want to run once for each feature in the features array.
-//   // Give each feature a popup that describes the place, time, and mag of the tornado.
-//   function onEachFeature(feature, layer) {
-//     const date = feature.properties.date;
-//     const time = feature.properties.time;
-//     const mag = feature.properties.mag;
+// PULL API DATA INTO JS
 
-//     // starting Lat and Lon and ending Lat and Lon
-//     const slat = feature.properties.slat;
-//     const slon = feature.properties.slon;
-//     const elat = feature.properties.elat;
-//     const elon = feature.properties.elon;
-//     const latlng = [feature.properties.elat, feature.properties.elon];
+// Perform a GET request to the chart query URL/
+function getChartJson(){
+    d3.json("/api/json").then(function (data) { 
+        createTable(data)
+    })
+}
 
-//     layer.bindPopup(`<h3>Tornado Details</h3>
-//       <p>Date: ${date}</p>
-//       <p>Time: ${time}</p>
-//       <p>Magnitude: ${mag}</p>
-//       <p>Coordinates: ${slat}, ${slon}</p>
-//     `);
-//   }
-//       // Determine the color of the marker based on the Magnitude.
+// Perform a GET request to the time query URL/
+function getTimeJson(){
+    timeURL = "/api/time"
+    d3.json(timeURL).then(function (data) {
+        createTime(data)
+    })
+}
 
-//     function getColor(mag) {
-//         if (mag <= 1) {
-//           return "#00FF00"; // green
-//         } else if (mag <= 1.5) {
-//           return "#ADFF2F"; // green-yellow
-//         } else if (mag <= 2) {
-//           return "#FFFF00"; // yellow
-//         } else if (mag <= 3) {
-//           return "#FFA500"; // orange
-//         } else if (mag <= 4) {
-//           return "#FF4500"; // orange-red
-//         } else {
-//           return "#FF0000"; // red
-//         }
-//       }
-//       // Define a function to create the markers for each tornado feature.
-//       function createMarker(feature) {
+// Perform a GET request to the state query URL
+function getStateJson(){
+    stateURL = "/api/state"
+    d3.json(stateURL).then(function (data) {
+        createState(data)
+    })
+}
 
-//         // Get the year from the feature's time property.
-//         var year = new Date(feature.properties.date).getFullYear();
-              
-//         // Check if the year matches the selected year.
-//         if (year === 2017) {
-//           // Determine the size of the marker based on the magnitude.
-//           var size = feature.properties.mag * 3;
-      
-//           // Create the marker object using the feature's coordinates.
-//           var marker = L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-//             color: "#000000",
-//             fillColor: getColor(feature.properties.mag),
-//             fillOpacity: 1.0,
-//             radius: size,
-//             weight: 1
-//           });
-      
-//           // Add a popup to the marker.
-//           marker.bindPopup(`<h3>${feature.properties.date}</h3><hr><p>${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Coordinates: ${[feature.properties.slat, feature.properties.slon]}</p>`);
-//           // Return the marker object.
-//           return marker;
-//         }
-//       }
-// // Create a GeoJSON layer that contains the features array on the tornadoData object.
-//   // Run the onEachFeature function once for each piece of data in the array.
-//   var tornados = L.geoJSON(tornadoData, {
-//     onEachFeature: onEachFeature,
-//     pointToLayer: createMarker
-//   });
-
-// // Create a legend control and add it to the map.
-// var legend = L.control({ position: 'bottomright' });
-// legend.onAdd = function (map) {
-//   var div = L.DomUtil.create('div', 'info legend');
-//   div.style.backgroundColor = 'darkgrey'; // Set background color to white
-  
-//   // Add HTML for legend with colored boxes
-//   div.innerHTML += '<i style="background:#00FF00"></i> <=1<br>';
-//   div.innerHTML += '<i style="background:#ADFF2F"></i> 1-1.5<br>';
-//   div.innerHTML += '<i style="background:#FFFF00"></i> 1.5-2<br>';
-//   div.innerHTML += '<i style="background:#FFA500"></i> 2-3<br>';
-//   div.innerHTML += '<i style="background:#FF4500"></i> 3-4<br>';
-//   div.innerHTML += '<i style="background:#FF0000"></i> 4+<br>';
-
-//   return div;
-// };
-
-//     // Send our tornados layer to the createMap function/
-//   createMap(tornados);
-
-//   function createMap(tornados) {
-//     console.log("createMap function called");
-//     // Create the base layers.
-
-//     var natGeo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-//         attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-//         maxZoom: 16
-//     });
-  
-//     var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//       attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-//     });
-  
-//     // Create a baseMaps object.
-//     var baseMaps = {
-//       "Topographic Map": topo,
-//       "National Geographic Map": natGeo
-//     };
-  
-//     // Create an overlay object to hold our overlay.
-//     var overlayMaps = {
-//       tornados: tornados
-//     };
-  
-//     // Create our map, giving it the streetmap and tornado layers to display on load.
-//     var myMap = L.map("map", {
-//       center: [
-//         37.09, -95.71
-//       ],
-//       zoom: 5,
-//       layers: [topo, tornados]
-//     });
-//     console.log(myMap);
-//     // Create a layer control.
-//     // Pass it our baseMaps and overlayMaps.
-//     // Add the layer control to the map.
-//     L.control.layers(baseMaps, overlayMaps, {
-//       collapsed: false
-//     }).addTo(myMap);
-//     console.log("Layer control added to map");
-// legend.addTo(myMap);
-//   }
-
+// function getStateJson2(){
+//     stateURL = "/api/state"
+//     d3.json(stateURL).then(function (data) {
+//         createState2(data)
+//     })
 // }
 
+yearSel.addEventListener("change", function () {
+    // Get the selected year from the year selector element.
+    selectedYear = yearSel.value;
+    let yearTitle = d3.select(".panel-title")
+    yearTitle.text(`Tornado Statistics for year ${selectedYear}`)
+    // d3.json("/api/json").then(function (data) { 
+    //     createTable(data)
+    // })
+    console.log(`call chart Json`)
+    getChartJson();
+    console.log(`call state Json`)
+    getStateJson();
+    console.log(`call show variables`)
+    showVariables(selectedYear, selectedChart);   
+});
 
+// console.log(`after function ${selectedYear}`)
+// showVariables(selectedYear, selectedChart);
+function showVariables(){
+    console.log(`show function ${selectedYear}`)
+    console.log(`show function ${selectedChart}`)
+}
 
-
-// Perform a GET request to the query URL/
-d3.json("/api/json").then(function (data) {
-
-    createTable(data)
-
-})
+// get chart type selection
+var chartSel = document.getElementById("but")
+chartSel.onclick = function () {
+    // Get the selected chart from the year selector element.
+    if(document.getElementById("Tornado").checked) {
+        selectedChart = 1}
+    if(document.getElementById("Mag").checked) {
+        selectedChart = 2}
+    if(document.getElementById("Loss").checked) {
+        selectedChart = 3}
+    // console.log(`inside function ${selectedChart}`)
+    // console.log(selectedYear)
+    // return selectedYear
+    getChartJson();
+    getTimeJson();
+    getStateJson();
+    showVariables(selectedYear, selectedChart);   
+};
 
 function createTable(chartData) {
-    console.log(chartData)
 
     // get data for summary table
     var totalLoss = 0
@@ -172,30 +99,31 @@ function createTable(chartData) {
     var maxLoss = 0
     var maxMag = 0
 
+    console.log(`create table year = ${selectedYear}`)
     for (var t=0; t < chartData.length; t++){
-        if (Object.values(chartData[t])[7] == 2017){
-            totalLoss = totalLoss + Object.values(chartData[t])[4],
-            totalInjuries = totalInjuries + Object.values(chartData[t])[2],
-            totalFatalities = totalFatalities + Object.values(chartData[t])[1],
-            totalLength = totalLength + Object.values(chartData[t])[3]
+        if (Object.values(chartData[t])[7] == selectedYear){
+            totalLoss = totalLoss + Object.values(chartData[t])[5],
+            totalInjuries = totalInjuries + Object.values(chartData[t])[3],
+            totalFatalities = totalFatalities + Object.values(chartData[t])[2],
+            totalLength = totalLength + Object.values(chartData[t])[4]
             totalTornados = totalTornados + 1
-            if (Object.values(chartData[t])[3] > maxLength){
-                maxLength = Object.values(chartData[t])[3]
+            if (Object.values(chartData[t])[4] > maxLength){
+                maxLength = Object.values(chartData[t])[4]
             }
             if (Object.values(chartData[t])[6] > maxWidth){
                 maxWidth = Object.values(chartData[t])[6]
             }
-            if (Object.values(chartData[t])[2] > maxInjuries){
-                maxInjuries = Object.values(chartData[t])[2]
+            if (Object.values(chartData[t])[3] > maxInjuries){
+                maxInjuries = Object.values(chartData[t])[3]
             }
-            if (Object.values(chartData[t])[1] > maxFatalities){
-                maxFatalities = Object.values(chartData[t])[1]
+            if (Object.values(chartData[t])[2] > maxFatalities){
+                maxFatalities = Object.values(chartData[t])[2]
             }
-            if (Object.values(chartData[t])[4] > maxLoss){
-                maxLoss = Object.values(chartData[t])[4]
+            if (Object.values(chartData[t])[5] > maxLoss){
+                maxLoss = Object.values(chartData[t])[5]
             }
-            if (Object.values(chartData[t])[5] > maxMag){
-                maxMag = Object.values(chartData[t])[5]
+            if (Object.values(chartData[t])[0] > maxMag){
+                maxMag = Object.values(chartData[t])[0]
             }
         }
     }
@@ -204,19 +132,6 @@ function createTable(chartData) {
     totalLossMillion = (totalLoss/1000000).toLocaleString('en-US', { maximumFractionDigits: 1 })
     maxLossMillion = (maxLoss/1000000).toLocaleString('en-US', { maximumFractionDigits: 1 })
     maxWidthMiles = maxWidth/1760
-    // console.log(`total_loss = ${totalLossMillion}`)
-    // console.log(`total_injuries = ${totalInjuries}`)
-    // console.log(`total_fatalities = ${totalFatalities}`)
-    // console.log(`total_length = ${totalLength}`)
-    // console.log(`total_tornados = ${totalTornados}`)
-    // console.log(`max_length = ${maxLength}`)
-    // console.log(`max_width = ${maxWidth}`)
-    // console.log(`max_injuries = ${maxInjuries}`)
-    // console.log(`max_fatalities = ${maxFatalities}`)
-    // console.log(`max_loss= ${maxLoss}`)
-    // console.log(`max_mag = ${maxMag}`)
-
-
 
     d3.select("#total-tornado").text(`Total Tornados: ${totalTornados}`)
     d3.select("#total-loss").text(`Total Loss: $${totalLossMillion} million`)
@@ -228,14 +143,214 @@ function createTable(chartData) {
     d3.select("#max-fatalities").text(`Maximum fatalities for a tornado: ${maxFatalities}`)
     d3.select("#max-length").text(`Maximum length for a tornado: ${maxLength.toLocaleString('en-US', { maximumFractionDigits: 1 })} miles`)
     d3.select("#max-width").text(`Maximum width for a tornado:${maxWidthMiles.toLocaleString('en-US', { maximumFractionDigits: 1 })} miles`)
+};
 
 
-    // get data for over time charts
+// get data for over time charts
+function createTime(timeData) {
 
-    // get list of years
+    // get lists for year, tornados, mag, & loss
     yearList=[]
+    tornadoYear=[]
+    magYear=[]
+    lossYear=[]
+
+    for (var y=0; y < timeData.length; y++){
+        let years = Object.values(timeData[y])[3]
+        yearList.push(years)
+
+        let tornados_year = Object.values(timeData[y])[2]
+        tornadoYear.push(tornados_year)
+
+        let mag_year = Object.values(timeData[y])[1]
+        magYear.push(mag_year)
+
+        let loss_year = Object.values(timeData[y])[0]
+        lossYear.push(loss_year)
+    }   
+
+    const ctx = document.getElementById("timeChart")
+
+    if(selectedChart === 1) {
+        chartLabel = `Tornados/Year`,
+        chartDataset = tornadoYear}
+
+    else if(selectedChart === 2) {
+        chartLabel = `Average Magnitude/Year`,
+        chartDataset = magYear}
+
+    else if(selectedChart === 3) {
+        chartLabel = `Property Loss/Year`,
+        chartDataset = lossYear}
+
+    console.log(`timeChartNew = ${timeChartNew}`)
+    console.log(timeChartNew)
+
+    if(timeChartNew === 1) {
+        console.log(`inside ChartNew if`)
+        timeChart.destroy()}
+    else
+        console.log('skipped destroy')
+
+
+    timeChart = new Chart(ctx,{
+        type: "bar",
+        data: {
+            labels: yearList,
+            datasets: [{
+                data:chartDataset,
+                borderWidth: 1,
+                backgroundColor: 'rgba(0,0,128)',
+                label: chartLabel
+            }]
+        }
+    })
+
+    timeChartNew = 1
+}
+
+
+// get data for state charts
+function createState(stateData) {
+    console.log(`create state chart year = ${selectedYear}`)
+    console.log(ChartNew)
+    state_year = []
+
+// filter data by year
+    console.log(stateData.length)
+    for (var s=0; s < stateData.length ; s++){
+        if (stateData[s].Year == selectedYear){
+            state_year.push(stateData[s])
+        }
+    }
+    console.log(state_year)
+
+
+// make new arrays sorted in descending order for each tornados, mag, & loss
+    let state_year_tornado = state_year.slice()
+    state_year_tornado = state_year_tornado.sort(function(obj1,obj2) {
+        return obj2.Tornados - obj1.Tornados
+    })
+    console.log(state_year_tornado)
     
 
+    let state_year_mag = state_year.slice()
+    state_year_mag = state_year_mag.sort(function(obj1,obj2) {
+        return obj2.Magnitude - obj1.Magnitude
+    })
+    console.log(state_year_mag)
 
 
-};
+    let state_year_loss = state_year.slice()
+    state_year_loss = state_year_loss.sort(function(obj1,obj2) {
+        return obj2.Loss - obj1.Loss
+    })
+    console.log(state_year_loss)
+
+
+// create lists for chart
+    stateCountList=[]
+    countListStates=[]
+
+    for (var i=0; i < 15; i++){
+        let states = state_year_tornado[i].State
+        stateCountList.push(states)
+
+        let count_states = state_year_tornado[i].Tornados
+        countListStates.push(count_states)
+    }
+    console.log(stateCountList)
+    console.log(countListStates)
+
+
+    stateMagList=[]
+    magListStates=[]
+
+    for (var m=0; m < 15; m++){
+        let states = state_year_mag[m].State
+        stateMagList.push(states)
+
+        let mag_states = state_year_mag[m].Magnitude
+        magListStates.push(mag_states)
+    }
+    console.log(stateMagList)
+    console.log(magListStates)
+
+
+    stateLossList=[]
+    lossListStates=[]
+    for (var l=0; l < 15; l++){
+        let states = state_year_loss[l].State
+        stateLossList.push(states)
+
+        let loss_states = state_year_loss[l].Loss
+        lossListStates.push(loss_states)
+    }
+    console.log(stateLossList)
+    console.log(lossListStates)
+
+
+    if(selectedChart === 1) {
+        chartLabel = `Top 15 States: Tornados/Year in ${selectedYear}`,
+        xAxis = stateCountList
+        yAxis = countListStates}
+
+    else if(selectedChart === 2) {
+        chartLabel = `Top 15 States: Average Magnitude/Year in ${selectedYear}`,
+        xAxis = stateMagList
+        yAxis = magListStates}
+
+    else if(selectedChart === 3) {
+        chartLabel = `Top 15 States: Property Loss/Year in ${selectedYear}`,
+        xAxis = stateLossList
+        yAxis = lossListStates}
+
+
+    console.log(`ChartNew = ${ChartNew}`)
+    console.log(ChartNew)
+
+    if(ChartNew === 1) {
+        console.log(`inside ChartNew if`)
+        stateChart.destroy()}
+    else
+        console.log('skipped destroy')
+
+
+    const ctx2 = document.getElementById("stateChart")
+
+    stateChart = new Chart(ctx2,{
+        type: "bar",
+        data: {
+            labels: xAxis,
+            datasets: [{
+                data:yAxis,
+                borderWidth: 1,
+                backgroundColor: 'rgba(128,0,128)',
+                label: chartLabel
+            }]
+        }
+    })
+
+    ChartNew = 1
+
+}
+
+function createYearSelector() {
+    // Code for dropdown menu that updates map
+    // Get a reference to the year selector element.
+    var yearSelector = document.getElementById("year-selector");
+  
+    // Loop through the years from 2007 to 2021.
+    for (var year = 2007; year <= 2021; year++) {
+        // Create an option element for the year.
+        var option = document.createElement("option");
+        option.value = year;
+        option.text = year;
+        // Append the option element to the year selector element.
+        yearSelector.appendChild(option);
+    }
+    return yearSelector
+  }
+
+
+
